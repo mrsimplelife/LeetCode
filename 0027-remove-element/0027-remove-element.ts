a2f786fdd52e9c1a077971a936bc74f72273f453
youme <mrsimplelife@naver.com>
2023-08-24 07:57:27 +0900

    Time: 58 ms (66.00%), Space: 43.5 MB (68.63%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.ts
@@ -0,0 +1,14 @@
+
+function removeElement(nums: number[], val: number): number {
+    let i = 0
+    let j = nums.length - 1;
+    while (i <= j) {
+        if (nums[i] !== val) {
+            i++;
+        } else {
+            nums[i] = nums[j];
+            j--;
+        }
+    }
+    return i
+};
