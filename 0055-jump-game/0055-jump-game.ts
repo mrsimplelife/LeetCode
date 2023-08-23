861610598bce6c25faab67f333ffdc166f5f039d
youme <mrsimplelife@naver.com>
2023-08-24 07:59:23 +0900

    Time: 75 ms (54.81%), Space: 48 MB (34.57%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.ts
@@ -0,0 +1,19 @@
+function canJump(nums: number[]): boolean {
+  
+  let index = 0;
+  while(index < nums.length - 1){
+      if(nums[index] === 0){
+        let back = 0
+        while(nums[index] <= back){
+          if(index === 0){
+            return false
+          }
+          index -= 1
+          back += 1
+        }
+      }
+      index += nums[index]
+  } 
+  return true
+};
+
