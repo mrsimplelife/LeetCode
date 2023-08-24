43b5a653eb16e88328644e912c834672aa2ccf66
youme <mrsimplelife@naver.com>
2023-08-25 08:10:40 +0900

    Time: 64 ms (67.89%), Space: 46.2 MB (41.38%) - LeetHub

--- 0169-majority-element/0169-majority-element.ts
+++ 0169-majority-element/0169-majority-element.ts
@@ -1,14 +1,15 @@
-
-
 function majorityElement(nums: number[]): number {
     const dict : Record<number,number> = {} 
+    
     for(let i = 0; i < nums.length; i++){
         const el = nums[i];
+        
         if (!dict[el]) {
             dict[el] = 1
         } else {
             dict[el] += 1
         }
     }
+		
     return parseInt(Object.entries(dict).sort((a,b) => b[1] - a[1])[0][0])
 }
