39a6d24b9429da77700f73f65ecbdc987d49358a
youme <mrsimplelife@naver.com>
2023-08-26 05:26:42 +0900

    Time: 67 ms (62.44%), Space: 44.7 MB (78.13%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.ts
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.ts
@@ -1,12 +1,16 @@
-function removeDuplicates(nums: number[]): number {
-    let index = 0;
-    for(let i = 0; i < nums.length ; i++){
-        if(nums[i] !== nums[index]){
-            nums[index] = nums[i]
-        }
-        if(nums[index] !== nums[index - 2]){
-            index += 1
+function removeDuplicates(nums) {
+    if (nums.length <= 2) {
+        return nums.length;
+    }
+
+    let index = 2;
+
+    for (let i = 2; i < nums.length; i++) {
+        if (nums[i] !== nums[index - 2]) {
+            nums[index] = nums[i];
+            index++;
         }
     }
-    return index
-};
+
+    return index;
+}
