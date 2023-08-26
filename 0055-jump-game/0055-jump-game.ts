853509ecefccd9bdc897e88c9fc98070b94ac298
youme <mrsimplelife@naver.com>
2023-08-26 23:03:23 +0900

    Time: 71 ms (65.71%), Space: 47.2 MB (78.85%) - LeetHub

--- 0055-jump-game/0055-jump-game.ts
+++ 0055-jump-game/0055-jump-game.ts
@@ -1,17 +1,11 @@
 function canJump(nums: number[]): boolean {
-  let index = 0;
-  while(index < nums.length - 1){
-      if(nums[index] === 0){
-        let back = 0
-        while(nums[index] <= back){
-          if(index === 0){
+    let max = 0
+    for(let i = 0; i < nums.length; i++){
+        if(i > max){
             return false
-          }
-          index -= 1
-          back += 1
         }
-      }
-      index += nums[index]
-  } 
-  return true
+        max = Math.max(max,i+nums[i])
+    }
+    return true
+    
 };
