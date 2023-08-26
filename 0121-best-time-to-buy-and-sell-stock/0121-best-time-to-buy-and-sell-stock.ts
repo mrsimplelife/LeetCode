b93c737cc59ae2b12729f5e0f4764ee712adbe1f
youme <mrsimplelife@naver.com>
2023-08-26 18:11:11 +0900

    Time: 78 ms (70.21%), Space: 51.7 MB (63.93%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.ts
@@ -1,9 +1,9 @@
 function maxProfit(prices: number[]): number {
-    let min = Infinity;
-    let profit = 0;
-    for (let i = 0; i < prices.length; i++){
-        profit = Math.max(prices[i] - min,profit)
-        min = Math.min(min,prices[i])
+    let min = Infinity
+    let maxProfit = 0
+    for(let i = 0; i < prices.length; i++){
+        min = Math.min(min, prices[i])
+        maxProfit = Math.max(maxProfit, prices[i] - min)
     }
-    return profit
+    return maxProfit
 };
