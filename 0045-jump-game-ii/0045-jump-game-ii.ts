71ca95aad71d414d4a8d71cf0ea171bee5e79f94
youme <mrsimplelife@naver.com>
2023-08-27 00:34:19 +0900

    Time: 142 ms (31.08%), Space: 45.1 MB (53.38%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.ts
+++ 0045-jump-game-ii/0045-jump-game-ii.ts
@@ -1,22 +1,17 @@
 function jump(nums: number[]): number {
   let count = 0;
-  let i = 0;
-  while (i < nums.length - 1) {
-    let next = nums[i] + i;
-    let max = next;
-    let j = next;
-    if (next >= nums.length - 1) {
-      return count + 1;
+  let jump = 0;
+  let max = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    const next = nums[i] + i
+    max = Math.max(max, next);
+    
+    if (i === jump) {
+      jump = max;
+      count++;
     }
-    while (j > i) {
-      if (max < nums[j] + j) {
-        max = nums[j] + j;
-        next = j;
-      }
-      j -= 1;
-    }
-    i = next;
-    count += 1;
   }
+
   return count;
 }
