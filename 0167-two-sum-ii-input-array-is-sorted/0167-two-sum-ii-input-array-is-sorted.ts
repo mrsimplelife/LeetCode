27e262e8f5c51df98bc3182ac7cad317798ff55f
youme <mrsimplelife@naver.com>
2023-08-27 01:13:51 +0900

    Time: 57 ms (84.14%), Space: 43.5 MB (70.63%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
@@ -0,0 +1,15 @@
+function twoSum(numbers: number[], target: number): number[] {
+  let i = 0;
+  let j = numbers.length - 1;
+  while (i < j) {
+    const sum = numbers[i] + numbers[j];
+    if (sum === target) {
+      return [i + 1, j + 1];
+    } else if (sum < target) {
+      i++;
+    } else {
+      j--;
+    }
+  }
+  return [];
+}
